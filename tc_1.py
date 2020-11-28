9fd8b3392caf15e3b283e87f1d86964043eb8578
yusuf44512 <65464755+yusuf44512@users.noreply.github.com>
2020-11-29 00:27:19 +0300

    Add files via upload

---
+++ tc_1.py
@@ -0,0 +1,23 @@
+import random
+
+adet=int(input("Kaç Adet TC kimlik No İstesiniz?"))
+i=1
+while i<=adet:
+    a1=random.randint(1,9)
+    a2=random.randint(0,9)
+    a3=random.randint(0,9)
+    a4=random.randint(0,9)
+    a5=random.randint(0,9)
+    a6=random.randint(0,9)
+    a7=random.randint(0,9)
+    a8=random.randint(0,9)
+    a9=random.randint(0,9)
+    a10=((((a1+a3+a5+a7+a9)*7)-(a2+a4+a6+a8))%10)
+    a11=((a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)%10)
+    print(i,".TC KİMLİK NO:",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11)
+    i=i+1
+    
+  
+print("NOT:!!!Numaraların aralarında boşluklar vardır!!!")
+
+
